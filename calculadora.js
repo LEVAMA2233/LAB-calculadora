ef7dcdc9a555216e56626d62ecb24da689c951c2
LEVAMA2233 <leopoldotrimex2233@gmail.com>
2025-11-19 14:05:42 -0600

    Fixed: Evitamos dividir entre cero en el codigo.

--- calculadora.js
+++ calculadora.js
@@ -13,6 +13,7 @@ function multiplica(x,y)
 }
 function divide(x,y)
 {
+    if(y==0){return "Syntax Error: Division entre cero"};
     return x/y;
 }
 function convierteString_a_Numero(a)
@@ -22,6 +23,7 @@ function convierteString_a_Numero(a)
 
 let a="5";
 let b="2";
+let n1="0";
 a=convierteString_a_Numero(a);
 b=convierteString_a_Numero(b);
 
@@ -34,5 +36,5 @@ console.log(`La resta de ${a} - ${b} es: ${op2}`);
 let op3=multiplica(a,b);
 console.log(`La multiplicacion de ${a}x${b} es: ${op3}`);
 
-let op4=divide(a,b);
-console.log(`La division de ${a}/${b} es: ${op4}`);
+let op4=divide(a,n1);
+console.log(`La division de ${a}/${n1} es: ${op4}`);
