65548d3bf0784f740121a12cb714b1e48938a44c
LEVAMA2233 <leopoldotrimex2233@gmail.com>
2025-11-19 13:20:44 -0600

    Agregamos las funcione para sumar,resta,dividir y multiplicar

---
+++ calculadora.js
@@ -0,0 +1,17 @@
+//Definimos a las funciones que haran las distintas operaciones
+function suma(x,y)
+{
+    return x+y;
+}
+function resta(x,y)
+{
+    return x-y;
+}
+function multiplica(x,y)
+{
+    return x*y;
+}
+function divide(x,y)
+{
+    return x/y;
+}
